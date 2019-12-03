2aac224854127f5c549324f2aedaab89a4d17bd8
François-Xavier Babin <francois-xavier.babin@outlook.fr>
2019-12-03 02:59:41 +0100

    Update python_goinfre_install.sh

--- python_goinfre_install.sh
+++ python_goinfre_install.sh
@@ -12,24 +12,24 @@ function set_conda {
     if echo $PYTHON_PATH | grep -q $INSTALL_PATH; then
 	    echo "good python version :)"   
     else
-	    cd
-	    if [ ! -f $SCRIPT ]; then
-		    curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	cd
+	if [ ! -f $SCRIPT ]; then
+		curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     	fi  
     	if [ ! -d $MINICONDA_PATH ]; then
 	    	sh $SCRIPT -b -p $INSTALL_PATH"/miniconda3"
-	    fi
-	    conda install -y "jupyter" "numpy" "pandas" "psycopg2"
-	    clear
-	    echo "Which python:"
-	    which python
-	    if grep -q "^export PATH=$MINICONDA_PATH" ~/.zshrc
-	    then
-	    	echo "export already in .zshrc";
-    	else
-		    echo "adding export to .zshrc ...";
-	    	echo "export PATH=$MINICONDA_PATH:$PATH" >> ~/.zshrc
-	    fi
-	    source ~/.zshrc
+	fi
+	conda install -y "jupyter" "numpy" "pandas" "psycopg2"
+	clear
+	echo "Which python:"
+	which python
+	if grep -q "^export PATH=$MINICONDA_PATH" ~/.zshrc
+	then
+		echo "export already in .zshrc";
+	else
+		echo "adding export to .zshrc ...";
+		echo "export PATH=$MINICONDA_PATH:$PATH" >> ~/.zshrc
+	fi
+	source ~/.zshrc
     fi
 }
