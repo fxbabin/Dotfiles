23f4d2e35c5826edf5ebd66fa5ea60f1de87b1f9
François-Xavier Babin <francois-xavier.babin@outlook.fr>
2019-12-03 02:58:01 +0100

    Update python_goinfre_install.sh

--- python_goinfre_install.sh
+++ python_goinfre_install.sh
@@ -1,38 +1,35 @@
 
 
 function set_conda {
-    REQUIREMENTS=jupyter jupyterlab numpy pandas matplotlib django psycopg2
 
-    MINICONDA_PATH=~/goinfre/miniconda3
-    export PATH="$MINICONDA_PATH/bin:$PATH"
+    HOME=$(echo ~)
+    INSTALL_PATH="/home/misteir"
+    MINICONDA_PATH=$INSTALL_PATH"/miniconda3/bin"
+    PYTHON_PATH=$(which python)
+    SCRIPT="Miniconda3-latest-Linux-x86_64.sh"
+    REQUIREMENTS="jupyter numpy pandas psycopg2"
 
-    login=$(whoami)
-    curr_python=$(which python)
-    mini_path=/Users/$login/goinfre/miniconda3/bin/python
-
-    if [ $curr_python = $mini_path ];
-    then
-        echo "good python version :)"
+    if echo $PYTHON_PATH | grep -q $INSTALL_PATH; then
+	    echo "good python version :)"   
     else
-        MINICONDA_PATH=~/goinfre/miniconda3
-        SCRIPT=Miniconda3-latest-MacOSX-x86_64.sh
-
-        curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-        sh $SCRIPT -b -p $MINICONDA_PATH
-        rm $SCRIPT
-
-        export PATH="$MINICONDA_PATH/bin:$PATH"
-        pip install $REQUIREMENTS
-        clear
-        echo "Which python:"
-        which python
-        if grep -q "export PATH=/Users/$login/goinfre/miniconda3/bin" ~/.zshrc
-        then
-           echo "export already in .zshrc";
-        else
-           echo "adding export to .zshrc ...";
-           echo "export PATH=/Users/$login/goinfre/miniconda3/bin" ~/.zshrc
-        fi
-        source ~/.zshrc
+	    cd
+	    if [ ! -f $SCRIPT ]; then
+		    curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    	fi  
+    	if [ ! -d $MINICONDA_PATH ]; then
+	    	sh $SCRIPT -b -p $INSTALL_PATH"/miniconda3"
+	    fi
+	    conda install -y "jupyter" "numpy" "pandas" "psycopg2"
+	    clear
+	    echo "Which python:"
+	    which python
+	    if grep -q "^export PATH=$MINICONDA_PATH" ~/.zshrc
+	    then
+	    	echo "export already in .zshrc";
+    	else
+		    echo "adding export to .zshrc ...";
+	    	echo "export PATH=$MINICONDA_PATH:$PATH" >> ~/.zshrc
+	    fi
+	    source ~/.zshrc
     fi
 }
