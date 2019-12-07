51303278eea7d66a98b84a25ef17e47e95e09a9d
François-Xavier Babin <francois-xavier.babin@outlook.fr>
2019-12-07 15:19:00 +0100

    Update python_goinfre_install.sh

--- python_goinfre_install.sh
+++ python_goinfre_install.sh
@@ -1,25 +1,23 @@
-
-
 function set_conda {
-
     HOME=$(echo ~)
-    INSTALL_PATH="/home/misteir"
+    INSTALL_PATH="/Users/fbabin"
     MINICONDA_PATH=$INSTALL_PATH"/miniconda3/bin"
     PYTHON_PATH=$(which python)
-    SCRIPT="Miniconda3-latest-Linux-x86_64.sh"
+    SCRIPT="Miniconda3-latest-MacOSX-x86_64.sh"
     REQUIREMENTS="jupyter numpy pandas psycopg2"
+    DL_LINK="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
 
     if echo $PYTHON_PATH | grep -q $INSTALL_PATH; then
-	    echo "good python version :)"   
+	    echo "good python version :)"
     else
 	cd
 	if [ ! -f $SCRIPT ]; then
-		curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    	fi  
+		curl -LO $DL_LINK
+    	fi
     	if [ ! -d $MINICONDA_PATH ]; then
 	    	sh $SCRIPT -b -p $INSTALL_PATH"/miniconda3"
 	fi
-	conda install -y "jupyter" "numpy" "pandas" "psycopg2"
+	conda install -y $(echo $REQUIREMENTS)
 	clear
 	echo "Which python:"
 	which python
