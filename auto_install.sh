ec493d12e0d959e29cc4e40a215b2a41c9fc27ad
François-Xavier Babin <francois-xavier.babin@outlook.fr>
2020-02-16 16:27:12 +0100

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+#rm -rf /sgoinfre/goinfre/Perso/fbabin/.brew && git clone --depth=1 https://github.com/Homebrew/brew /sgoinfre/goinfre/Perso/fbabin/.brew && export PATH=/sgoinfre/goinfre/Perso/fbabin/.brew/bin:$PATH && brew update && echo "export PATH=/sgoinfre/goinfre/Perso/fbabin/.brew/bin:$PATH" >> ~/.zshrc
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -L https://raw.githubusercontent.com/fxbabin/Dotfiles/master/.vimrc > ~/.vimrc
